4a656ef383dc3e11e66e7b3602400ff48c30cff6
Olga <xotaoshi@gmail.com>
2025-06-13 20:54:57 +0300

    Add project files.

---
+++ Task2/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Task2
+{/*
+  * Задание 2
+Пользователь вводит словами цифру от 0 до 9. Приложение должно перевести слово в цифру. Например, если
+пользователь ввёл five, приложение должно вывести на
+экран 5.
+  */
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введте словами цифру от 0 до 9. Приложение должно перевести слово в цифру.");
+            string UserInput = Input.UserInput.GetStringFromUser();
+            int? num = Task2.ConverterStoI(UserInput);
+            if (num is int val)
+            {
+                Console.WriteLine(num);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+
+
+        }
+    }
+}
---
+++ Task2/Task2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+  Задание 2
+Пользователь вводит словами цифру от 0 до 9. Приложение должно перевести слово в цифру. Например, если
+пользователь ввёл five, приложение должно вывести на
+экран 5.
+  */
+namespace Task2
+{
+    internal class Task2
+    {
+        internal static int? ConverterStoI(string source)
+        {
+            int num = -1;
+            bool check = false;
+            try
+            {
+                source = source.ToLower();
+
+                switch (source)
+                {
+                    case "ноль":
+                    case "zero":
+                        num = 0;
+                        break;
+                    case "один":
+                    case "one":
+                        num = 1;
+                        break;
+                    case "два":
+                    case "two":
+                        num = 2;
+                        break;
+                    case "три":
+                    case "three":
+                        num = 3;
+                        break;
+                    case "четыре":
+                    case "four":
+                        num = 4;
+                        break;
+                    case "пять":
+                    case "five":
+                        num = 5;
+                        break;
+                    case "шесть":
+                    case "six":
+                        num = 6;
+                        break;
+                    case "семь":
+                    case "seven":
+                        num = 7;
+                        break;
+                    case "восемь":
+                    case "eight":
+                        num = 8;
+                        break;
+                    case "девять":
+                    case "nine":
+                        num = 9;
+                        break;
+                    default:
+                        check = true;
+                        throw new Exception("Необрабатываемая надпись");
+                        break;
+                }
+
+                return num;
+            }
+            catch (Exception ex) when (check== true)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
